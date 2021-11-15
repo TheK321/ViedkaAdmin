216840300d6262b0e34e1df8f2c27862d50f2964
thek321 <thek321xd@gmail.com>
2021-11-15 14:17:00 -0600

    Agregadas dependencias material design 3 Agregado NavigationRail y sus iconos

--- app/src/main/java/com/example/viedkaadmin/MainActivity.java
+++ app/src/main/java/com/example/viedkaadmin/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.viedkaadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigationrail.NavigationRailView;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavigationRailView menuViedka;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    Fragment fragment=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuViedka=findViewById(R.id.id_menuviedka);
+        if(fragment==null){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout_container, new FragmentPantallaVentas());
+            fragmentTransaction.commit();
+        }
+
+        menuViedka.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.menu_resumen:
+                        //fragment = new fragmentPantallaResumen;
+                        cambiarTituloBarra("Resumen");
+                        break;
+                    case R.id.menu_ventas:
+                        fragment = new FragmentPantallaVentas();
+                        cambiarTituloBarra("Ventas");
+                        break;
+                    case R.id.menu_agregarreporte:
+                        fragment = new FragmentPantallaAgregarReporte();
+                        cambiarTituloBarra("Agregar Reporte");
+                        break;
+                    case R.id.menu_librocontable:
+                        //fragment = new FragmentPantallaLibroContable();
+                        cambiarTituloBarra("Libro Contable");
+                        break;
+                }
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_layout_container, fragment);
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
+    }
+
+    public void cambiarTituloBarra(String nuevoTitulo){
+        getSupportActionBar().setTitle(nuevoTitulo);
     }
 }
