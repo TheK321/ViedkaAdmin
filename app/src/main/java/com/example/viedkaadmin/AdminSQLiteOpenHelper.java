f19154204db811635b6744cc651687f77348dd6f
thek321 <thek321xd@gmail.com>
2021-11-24 13:17:24 -0600

    Agregado SQLiteOpenHelper

---
+++ app/src/main/java/com/example/viedkaadmin/AdminSQLiteOpenHelper.java
@@ -0,0 +1,43 @@
+package com.example.viedkaadmin;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+//SQLiteOpenHelper genera los metodos onCreate onUpgrade y AdminSQLiteOpenHelper
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDeDatos/*Nombre de la Base de Datos*/) {
+        //Creacion de la tabla prendas en la BaseDeDatos
+        BaseDeDatos.execSQL("create table Prendas"/*Nombre de la table*/ +
+                "(idPrenda integer primary key autoincrement not null unique, " +
+                "Nombre text not null,Descripcion text," +
+                "Categoria text, Cantidad int not null, " +
+                "Precio real not null)");/*Columnas de la tabla*/
+
+        BaseDeDatos.execSQL("create table Compra"/*Nombre de la table*/ +
+                "(idCompra integer primary key autoincrement not null unique, " +
+                "NombreProd text not null,Descripcion text," +
+                "Cantidad integer not null, " +
+                "PrecioUnitario real not null," +
+                "MontoTotal real not null)");/*Columnas de la tabla*/
+
+        BaseDeDatos.execSQL("create table Compra_Prendas"/*Nombre de la table*/ +
+                "(Compra_idCompra integer references Compra (idCompra) ON DELETE RESTRICT ON UPDATE RESTRICT NOT NULL," +
+                "Prendas_idPrenda integer references Prendas (idPrenda) ON DELETE RESTRICT ON UPDATE RESTRICT NOT NULL," +
+                "PRIMARY KEY (Compra_idCompra, Prendas_idPrenda))"); /*Columnas de la tabla*/
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
+
